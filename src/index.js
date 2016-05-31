7df918166cce581030c9a4c5d20659c3e4d218c3
skaxwng <samkj.ks@gmail.com>
2016-05-31 09:44:40 -0500

    Provide a way to either specify an options object or a function that has context. fixed issue #1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import { compose } from 'react-komposer';
 import gql from 'apollo-client/gql';
 
-export function composeQueryBase(options, fn, props, onData) {
-  if (options === null || typeof options !== 'object') {
-    const error = 'No options object passed as argument';
+export function composeQueryBase(optionsOrFn, fn, props, onData) {
+  if (!props.context) {
+    const error = 'No context passed as prop';
     throw new Error(error);
+  }
+
+  const context = typeof props.context === 'function' ? props.context() : props.context;
+
+  const Client = context.Client || context.client;
+
+  if (!Client) {
+    const error = 'No Apollo Client found in the context';
+    throw new Error(error);
+  }
+
+  if (optionsOrFn === null || optionsOrFn === undefined) {
+    const error = 'Either an option object or a function should be provided';
+    throw new Error(error);
+  }
+
+  let options = {};
+  if (typeof optionsOrFn === 'object') {
+    options = optionsOrFn;
+  } else if (typeof optionsOrFn === 'function') {
+    options = optionsOrFn(context);
   }
 
   if (!options.hasOwnProperty('query')) {
@@ -19,20 +40,6 @@ export function composeQueryBase(options, fn, props, onData) {
 
   if (fn === null || typeof fn !== 'function') {
     const error = 'No resultMapper function passed as argument';
-    throw new Error(error);
-  }
-
-  if (!props.context) {
-    const error = 'No context passed as prop';
-    throw new Error(error);
-  }
-
-  const context = typeof props.context === 'function' ? props.context() : props.context;
-
-  const Client = context.Client || context.client;
-
-  if (!Client) {
-    const error = 'No Apollo Client found in the context';
     throw new Error(error);
   }
 
@@ -56,6 +63,6 @@ export function composeQueryBase(options, fn, props, onData) {
   });
 }
 
-export default function composeWithQuery(query, fn) {
-  return compose(composeQueryBase.bind(null, query, fn));
+export default function composeWithQuery(optionsOrFn, fn) {
+  return compose(composeQueryBase.bind(null, optionsOrFn, fn));
 }
